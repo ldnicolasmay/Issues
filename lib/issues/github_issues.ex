aa1a9ef795155f0899063e2a39d555befca1f62a
ldnicolasmay <ldnicolasmay@gmail.com>
2021-05-21 23:47:21 -0400

    Clean up lib/issues/github_issues.ex

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -9,20 +9,9 @@ defmodule Issues.GithubIssues do
     |> handle_response()
   end
 
-  # def issues_url(user, project) do 
-  #   "https://api.github.com/repos/#{user}/#{project}/issues"
-  # end
   def issues_url(user, project) do 
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
-
-  # def handle_response({:ok, %{status_code: 200, body: body}}) do 
-  #   {:ok, body}
-  # end
-
-  # def handle_response({_, %{status_code: _, body: body}}) do 
-  #   {:error, body}
-  # end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do 
     {
